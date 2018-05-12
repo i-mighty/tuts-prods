55e47e4a43a6227186eb77b4c1b0e13c84edc40c
Adegboye Josiah <mighty@localhost.localdomain>
2018-05-12 18:52:27 +0100

    conditional secure

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,7 +31,7 @@ class AppServiceProvider extends ServiceProvider
 	    	'admin' => 'App\Admin',
 		    'user' => 'App\User',
 	    ]);
-	    if(env('APP_ENV') == 'production'){
+	    if(env('APP_ENV') == 'production' || env('APP_ENV') == 'stage'){
 	    	URL::forceSchema('https');
 	    }
     }
